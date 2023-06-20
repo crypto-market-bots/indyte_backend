407db9d7814234aa5093a7684b672dadcccb58eb
deepak01-hacker <a9649060356@gmail.com>
2023-06-20 23:59:40 +0530

    meal planner added

---
+++ mealPlanner/controller.js
@@ -0,0 +1,71 @@
+const {Meal, UserMealRecommendation} = require('./model');
+
+exports.allMealsFetch = catchAsyncError(async(req,res, next) => {
+ try {
+    const meals = await Meal.find();
+    res.status(201).json({
+        success: true,
+        data:meals,
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch meals' });
+  }
+});
+
+exports.userMealRecommendation = catchAsyncError(async(req,res, next) => {  // Api for to add meal : dietition
+    try {
+        var { user_id, meal_id, meal_time, meal_period} = req.body; // meal_time : DD-MM-YYYY
+        if (!meal_id) {
+            const { meal_name, nutritions, description, image_link, required_ingredients, steps} = req.body;
+            const meal = new Meal({
+                name:meal_name,
+                nutritions:nutritions,
+                description:description,
+                image:image_link,
+                required_ingredients:required_ingredients,
+                steps:steps,
+                created_by:req.user.id
+            });
+            const savedMeal = await meal.save();
+            meal_id = savedMeal._id;
+        }
+
+        const meal_time_formatted =  moment(meal_time, 'DD-MM-YYYY').toDate();
+        const userMealRecommendation = new UserMealRecommendation({
+            user:user_id, // customer 
+            meal:meal_id,
+            meal_time : meal_time_formatted,
+            meal_period: meal_period,
+            added_by:req.user.id
+        });
+    
+        await userMealRecommendation.save();
+        res.status(201).json({success: true,message: "Success"});
+      } catch (error) {
+        res.status(500).json({ error: 'Failed to create user meal recommendation' });
+      }
+});
+
+exports.userMealRecommendationFetch = catchAsyncError(async(req,res, next) => {
+    try {
+        if (req.query.meal_id) {
+            const meal = await Meal.findById(req.query.meal_id);
+            res.status(201).json({success: true, data:meal});
+        } else {
+            const mealLogs = await UserMealRecommendation.find().populate('user', 'name').populate('meal', 'name');
+            res.status(201).json({success: true, data:mealLogs});
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch user meal recommendations' });
+    }
+});
+
+exports.userMealRecommendationDelete = catchAsyncError(async(req,res, next) => {
+    try {
+        const meal_id = req.query.meal_id;
+        const meal = await Meal.MyModel.deleteOne({ _id: meal_id })
+        res.status(201).json({success: true,message: "Success"});
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete user meal recommendations' });
+    }
+});
---
+++ mealPlanner/model.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+
+const mealSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  nutritions: { // {"fat": {type: , value: }, ""}
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  required_ingredients: { // {"water": {value: 4, type : cup}}
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+  },
+  steps: {  // {"1": "abcdefghijklmnop", "2":"abcdefghijklmnop" .....}
+    type: mongoose.Schema.Types.Mixed,
+    required: true,
+  },
+  created_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  created_time: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const userMealRecommendationSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    meal: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Meal',
+        required: true,
+    },
+    meal_time: {
+        type: Date,
+        required: true,
+    },
+    meal_period: {
+        type: String,
+        enum: ['lunch', 'breakfast', 'dinner'],
+        required: true,
+    },
+    user_picked: {
+        type: Boolean,
+        required: true,
+    },
+    added_by: { // dietition 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    created_time: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const UserMealRecommendation = mongoose.model('UserMealRecommendation', userMealRecommendationSchema);
+const Meal = mongoose.model('Meal', mealSchema);
+
+module.exports = Meal;
+module.exports = UserMealRecommendation;
