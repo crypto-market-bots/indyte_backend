f5d61dc5245a4943e11c0806a132caec0cffb866
Mohit Soni <mohitsonims04@gmail.com>
2023-10-03 01:19:38 +0530

    history api handled sucussfully

--- routes/common.js
+++ routes/common.js
@@ -1,5 +1,18 @@
-const express = require('express');
-const { sendOTP } = require('../Common/sendOtp');
+const express = require("express");
+const { sendOTP,getHistory } = require("../Common/sendOtp");
+
+const { isAuthenticated, authorizedRoles } = require("../middleware/auth");
+
+
 const router = express.Router();
 router.route("/sendotp").post(sendOTP);
+
+router
+  .route("/get-history")
+  .get(
+    isAuthenticated("web"),
+    authorizedRoles("admin", "dietitian"),
+    getHistory
+  );
+
 module.exports = router;
