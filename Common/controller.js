84e6e5ac3abc078dce3d5d2e584d20e593bd96b2
zarvish <codzviston@gmail.com>
2023-11-07 01:26:17 +0545

    populate function added for assigned by in history table, changes are in the get-hisoty api only

--- Common/controller.js
+++ Common/controller.js
@@ -3,7 +3,7 @@ const catchAsyncError = require("../middleware/catchAsyncError");
 const User = require("../users/model");
 const { Meal, UserMealRecommendation } = require("../mealPlanner/model");
 const { Workout, workoutRecommendation } = require("../workoutTracker/model");
-const { ObjectId } = require('mongodb');
+const { ObjectId } = require("mongodb");
 const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
@@ -13,7 +13,7 @@ const jwt = require("jsonwebtoken");
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_TOKEN;
-console.log(accountSid,authToken)
+console.log(accountSid, authToken);
 const client = require("twilio")(accountSid, authToken);
 
 const crypto = require("crypto");
@@ -23,45 +23,38 @@ const { uploadAndPushImage } = require("./uploadToS3");
 const smsKey = process.env.SMS_SECRET_KEY;
 const twilioNum = process.env.TWILIO_PHONE_NUMBER;
 
-
-
-
 exports.sendOTP = catchAsyncError(async (req, res, next) => {
   // const phone = req.phone;
-  const phone = "+91"+ req.phone;
+  const phone = "+91" + req.phone;
   // phone =+phone;
-  console.log("these are envs",accountSid,authToken)
+  console.log("these are envs", accountSid, authToken);
 
   const otp = Math.floor(100000 + Math.random() * 900000);
-  
+
   const ttl = 5 * 60 * 1000;
-const expires = Date.now() + ttl;
-const data = `${phone}.${otp}.${expires}`;
-const hash = crypto.createHmac("sha256", smsKey).update(data).digest("hex");
-const fullHash = `${hash}.${expires}`;
-console.log("hash",twilioNum,phone)
-client.messages
-  .create({
-    body: `Your One Time Login Password For Indyte is ${otp} . Valid only for 2 minutes`,
-    from: twilioNum,
-    friendlyName: "My First Verify Service",
-    to: phone,
-  })
-  .then((messages) => {
-    res.status(200).send({ phone, hash: fullHash });
-  })
-  .catch((err) => {
-    console.log(err)
-    return next(new ErrorHander(err, 400));
-  });
+  const expires = Date.now() + ttl;
+  const data = `${phone}.${otp}.${expires}`;
+  const hash = crypto.createHmac("sha256", smsKey).update(data).digest("hex");
+  const fullHash = `${hash}.${expires}`;
+  console.log("hash", twilioNum, phone);
+  client.messages
+    .create({
+      body: `Your One Time Login Password For Indyte is ${otp} . Valid only for 2 minutes`,
+      from: twilioNum,
+      friendlyName: "My First Verify Service",
+      to: phone,
+    })
+    .then((messages) => {
+      res.status(200).send({ phone, hash: fullHash });
+    })
+    .catch((err) => {
+      console.log(err);
+      return next(new ErrorHander(err, 400));
+    });
 
   // If none of the error conditions are met, proceed with sending OTP
   // next();
-
-  
 });
-
-
 
 exports.getHistory = catchAsyncError(async (req, res, next) => {
   try {
@@ -70,8 +63,8 @@ exports.getHistory = catchAsyncError(async (req, res, next) => {
     const per_page = parseInt(req.query.perPage) || 10;
     const skip = (page - 1) * per_page;
 
-    console.log("body",req.body)
-    
+    console.log("body", req.body);
+
     if (!user_id || !type) {
       return next(new ErrorHander("All field are required ", 400));
     }
@@ -80,35 +73,35 @@ exports.getHistory = catchAsyncError(async (req, res, next) => {
       return next(new ErrorHander("Invalid user_id", 400));
     }
 
-    let historyData
+    let historyData;
 
     let DatabaseName;
-    if(type==="workout"){
-      console.log("into workout Block")
-      DatabaseName=workoutRecommendation
-      populationKeyName='workout_id'
-    }
-    else if(type==="meal"){
-      console.log("into Meal Block")
-      
-      DatabaseName=UserMealRecommendation
-      populationKeyName='meal'
-    }
-    else{
-      console.log("into meal Block")
-      DatabaseName=UserMealRecommendation
-      populationKeyName='meal'
+    if (type === "workout") {
+      console.log("into workout Block");
+      DatabaseName = workoutRecommendation;
+      populationKeyName = "workout_id";
+    } else if (type === "meal") {
+      console.log("into Meal Block");
+
+      DatabaseName = UserMealRecommendation;
+      populationKeyName = "meal";
+    } else {
+      console.log("into meal Block");
+      DatabaseName = UserMealRecommendation;
+      populationKeyName = "meal";
     }
 
     const totalResultLength = await DatabaseName.countDocuments({
       $or: [{ user: user_id }],
     });
-    
+
     historyData = await DatabaseName.find({
       $or: [{ user: user_id }],
-    }).populate(populationKeyName)
-    .skip(skip) // Skip the appropriate number of documents
-    .limit(per_page);
+    })
+      .populate(populationKeyName)
+      .populate("assigned_by", "first_name last_name")
+      .skip(skip) // Skip the appropriate number of documents
+      .limit(per_page);
 
     res.status(200).json({
       success: true,
@@ -121,11 +114,7 @@ exports.getHistory = catchAsyncError(async (req, res, next) => {
   }
 });
 
-
-exports.uploadImage = catchAsyncError(async(req,res,next)=>{
-
- 
-
+exports.uploadImage = catchAsyncError(async (req, res, next) => {
   const data = await uploadAndPushImage(
     "default/profile_image",
     req.files.image,
@@ -133,6 +122,4 @@ exports.uploadImage = catchAsyncError(async(req,res,next)=>{
     "indyte@example.com"
   );
   console.log(data);
-})
-
-
+});
